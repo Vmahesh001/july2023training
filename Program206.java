14b92f86b3bf5bb091d7cd04ad62c083c452c1e2
Vmahesh001 <131321550+Vmahesh001@users.noreply.github.com>
2023-06-02 17:51:19 +0530

    Add files via upload

---
+++ Program206.java
@@ -0,0 +1,18 @@
+// ascii values
+import java.util.*;
+public class Program206
+{
+public static void main(String args[])
+{
+char ch;
+Scanner sc=new Scanner(System.in);
+ch=sc.next().charAt(0);
+int ascii=(int)ch;
+System.out.println("the ascii value of"+ch+"is"+ascii);
+}
+}
+
+
+
+
+
